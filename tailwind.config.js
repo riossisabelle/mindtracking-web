b818580209badabcf826ffc8ec601fb585564b88
davidapaz05 <davipaz125@gmail.com>
2025-08-06 10:16:27 -0300

    adicionando o projeto base

---
+++ tailwind.config.js
@@ -0,0 +1,17 @@
+module.exports = {
+  content: [
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {
+
+        colors: {
+            'kaika': '#fff',
+            'custom-green': '#10b981',
+            'custom-red': '#ef4444',
+        },
+
+    },
+  },
+  plugins: [],
+}
