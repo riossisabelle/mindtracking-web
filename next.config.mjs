c77efb00fa0bca71fa0d902b6f7f0fa9d3cfe222
davidapaz05 <davipaz125@gmail.com>
2025-08-22 09:20:32 -0300

    atualizando o dockerfile juntamente com o packjson

--- next.config.mjs
+++ next.config.mjs
@@ -1,13 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-
-  // Gera build leve para Docker
-  output: 'standalone',
-
-  experimental: {
-    typedRoutes: false,
-  },
 };
 
 export default nextConfig;
