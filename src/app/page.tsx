7749ba681f4859fd37ef1ba52de9c3898d066620
riossisabelle <isabelleriossousa@gmail.com>
2025-09-17 09:54:46 -0300

    tela de questionario finalizada

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,48 +1,18 @@
 "use client";
 
-import { useState } from "react";
 import { useTheme } from "../contexts/ThemeContext";
 import Sidebar from "../components/layout/Sidebar";
-import ForgotPasswordModal from "../components/features/Auth/RedefiniçãoSenha/VerificacaoEmail";
+import Questionnaire from "./(private)/Questionnaire";
 
 export default function Home() {
-  const { theme } = useTheme(); // Obtém o tema atual do contexto
-  const [isModalOpen, setIsModalOpen] = useState(false);
-
-  const handleModalSuccess = () => {
-    console.log("Modal de sucesso foi chamado!");
-    setIsModalOpen(false);
-    // Aqui você pode abrir o modal 2 se quiser
-  };
+  const { theme } = useTheme(); // Obtém o tema atual
 
   return (
-    <div
-      className={`min-h-screen flex items-center justify-center ${
-        theme === "dark" ? "bg-gray-900" : "bg-gray-100"
-      }`}
-    >
-      <div className="text-center space-y-4">
-        <Sidebar />
-        <h1
-          className={`text-2xl font-bold ${
-            theme === "dark" ? "text-white" : "text-gray-800"
-          }`}
-        >
-          Teste do Modal
-        </h1>
-        <button
-          onClick={() => setIsModalOpen(true)}
-          className="bg-blue-600 hover:bg-blue-700 text-white font-semibold py-3 px-6 rounded-lg transition-colors"
-        >
-          Abrir Modal de Verificação de Email
-        </button>
-      </div>
-
-      <ForgotPasswordModal
-        isOpen={isModalOpen}
-        onClose={() => setIsModalOpen(false)}
-        onSuccess={handleModalSuccess}
-      />
+    <div>
+      <Sidebar />
+      <main className="ml-0 lg:ml-34.5 ">
+          <Questionnaire theme={theme} />
+      </main>
     </div>
   );
 }
