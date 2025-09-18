3677d610d6de06401ede1bf8b545e4a6585412e6
Pedro <phmoraes.alves@gmail.com>
2025-09-18 18:08:41 -0300

    arquivo page.tsx retirado para subida de PR

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,9 @@
 "use client";
 import { useTheme } from "../contexts/ThemeContext";
-import Sidebar from "../components/layout/Sidebar";
-import ForgotPasswordButton from "@/components/common/Buttons/ButtonEsqueceuSenha";
+
 
 export default function Home() {
-  const { theme } = useTheme(); // Obtém o tema atual do contexto
+  const { theme } = useTheme();
 
 
   return (
@@ -13,17 +12,6 @@ export default function Home() {
         theme === "dark" ? "bg-gray-900" : "bg-gray-100"
       }`}
     >
-      <div className="text-center space-y-4">
-        <Sidebar />
-        <h1
-          className={`text-2xl font-bold ${
-            theme === "dark" ? "text-white" : "text-gray-800"
-          }`}
-        >
-          Teste do Modal
-        </h1>
-        <ForgotPasswordButton />
-      </div>
     </div>
   );
 }
