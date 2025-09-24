c98f939474ad3ae4a39c3b7b1e03cd6c3447dba2
PedroMoraes1 <phmoraes.alves@gmail.com>
2025-09-24 11:47:12 -0300

    Tela de dashboard em andamento

--- src/components/common/Cards/Cards_Dashboard/BaseCard.tsx
+++ src/components/common/Cards/Cards_Dashboard/BaseCard.tsx
@@ -1,14 +1,13 @@
+"use client";
 import { ReactNode } from "react";
 
 interface BaseCardProps {
-  title: string;
   children: ReactNode;
 }
 
-export default function BaseCard({ title, children }: BaseCardProps) {
+export default function BaseCard({ children }: BaseCardProps) {
   return (
-    <div className="bg-gray-800 rounded-2xl shadow-lg p-5 border border-gray-700 hover:border-blue-500 transition">
-      <h2 className="text-lg font-semibold mb-3">{title}</h2>
+    <div className="bg-gray-800 rounded-[8px] pl-[21px] pr-[33px] pt-[25px] max-w-[384px]">
       {children}
     </div>
   );
